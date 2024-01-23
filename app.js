decea5d34b7c3655e2c1ebf6a93d09fb506a7745
Bhavya Antiya <bhavyaantiya@gmail.com>
2024-01-23 15:00:23 -0500

    This will give you error

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log("Hello World!!! ");
+function add(n1, n2) {
+    return n1 + n2;
+}
+var number1 = 3;
+var number2 = '90';
+console.log(add(number1, number2));
--- app.ts
+++ app.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!!! ");
+function add(n1: number, n2: number) {
+    return n1+n2
+}
+
+const number1 = 3
+const number2 = '90'
+
+// ERROR
+console.log(add(number1, number2))  //  Argument of type 'string' is not assignable to parameter of type 'number'.
